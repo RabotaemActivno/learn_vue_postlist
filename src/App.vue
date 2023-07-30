6b0bd19ccabb03116c720f0fbf827df365aae284
RabotaemActivno <fajzovigor5@gmail.com>
2023-07-30 19:14:31 +0300

    2 components

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="app">
+    <PostForm @create="createPost"/>
+    <PostList :posts="posts"/>
+  </div>
+</template>
+
+<script>
+import PostForm from "./components/PostForm.vue";
+import PostList from "./components/PostList.vue";
+export default {
+  components: {
+    PostForm,
+    PostList,
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "JavaScript", body: "Описание поста" },
+        { id: 2, title: "JavaScript 2", body: "Описание поста 2" },
+        { id: 3, title: "JavaScript 3", body: "Описание поста 3" },
+      ],
+    };
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post)
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+</style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,64 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+      v-model="post.title"
+      class="input"
+      type="text"
+      placeholder="Название поста"
+    />
+    <input
+      v-model="post.body"
+      class="input"
+      type="text"
+      placeholder="Описание поста"
+    />
+    <button @click="createPost" class="btn">Создать</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now()
+      this.$emit('create', this.post)
+      this.post = {
+        title: '',
+        body: ''
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+  cursor: pointer;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+</style>
